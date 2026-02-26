41da73f9a745b5e5a33fca2850f01cb734f1dcad
Gurkan <hello@gurkz.me>
2026-02-26 13:19:15 +0100

    move navbar into its own component (will be used later)

---
+++ src/lib/components/navbar.svelte
@@ -0,0 +1,20 @@
+<script>
+	import { useSession } from "$lib/session.svelte";
+	import ModeToggle from "./mode-toggle.svelte";
+	import { Button } from "$lib/components/ui/button/index.js";
+
+	const session = useSession();
+</script>
+
+<div class="flex flex-row items-center gap-3">
+	<ModeToggle />
+
+	{#if session.current?.user}
+		<p>hi, {session.current.user.name}!</p>
+		<Button class="pl-0" variant="link" onclick={() => session.signOut()}>sign out</Button>
+	{:else}
+		<Button class="pl-0" variant="link" onclick={() => session.signInSocial("discord")}
+			>sign in with Discord</Button
+		>
+	{/if}
+</div>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,9 +2,8 @@
 	import "./layout.css";
 	import favicon from "$lib/assets/favicon.svg";
 	import { ModeWatcher } from "mode-watcher";
-	import { SessionState, setSession, useSession } from "$lib/session.svelte.js";
-	import ModeToggle from "$lib/components/mode-toggle.svelte";
-	import { Button } from "$lib/components/ui/button/index.js";
+	import { SessionState, setSession } from "$lib/session.svelte.js";
+	import Navbar from "$lib/components/navbar.svelte";
 
 	let { children, data } = $props();
 
@@ -12,30 +11,13 @@
 	let sessionState = new SessionState($state.snapshot(data.session));
 
 	setSession(sessionState);
-
-	const session = useSession();
 </script>
 
 <svelte:head><link rel="icon" href={favicon} /></svelte:head>
 <ModeWatcher defaultMode="dark" disableTransitions />
 
-{#snippet authStatus()}
-	<div class="flex flex-row items-center gap-3">
-		<ModeToggle />
-
-		{#if session.current?.user}
-			<p>hi, {session.current.user.name}!</p>
-			<Button class="pl-0" variant="link" onclick={() => session.signOut()}>sign out</Button>
-		{:else}
-			<Button class="pl-0" variant="link" onclick={() => session.signInSocial("discord")}
-				>sign in with Discord</Button
-			>
-		{/if}
-	</div>
-{/snippet}
-
 <div class="min-h-screen p-4">
-	{@render authStatus()}
+	<Navbar />
 	<div class="pt-4">
 		<section class="flex min-h-[90vh] justify-center rounded-xl bg-gray-950 py-5 text-white">
 			<div class="w-full max-w-7xl px-6">
