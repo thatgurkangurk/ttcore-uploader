1163005bfea7d7752dc7c3f15b5d60f4a3c94f42
Gurkan <hello@gurkz.me>
2026-03-01 16:25:57 +0100

    if global_name isn't set, use the username

--- src/lib/server/auth.ts
+++ src/lib/server/auth.ts
@@ -24,7 +24,7 @@ export const auth = betterAuth({
 			mapProfileToUser: async (profile) => {
 				return {
 					username: profile.username,
-					name: profile.global_name
+					name: profile.global_name || profile.username
 				};
 			}
 		}
