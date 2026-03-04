16b729b91819b73b004f9fc45021bcee366fe161
Gurkan <hello@gurkz.me>
2026-03-04 12:47:45 +0100

    add name and submissionsOpen to video table

--- src/lib/server/db/schema/video.ts
+++ src/lib/server/db/schema/video.ts
@@ -1,5 +1,7 @@
 import { pgTable } from "drizzle-orm/pg-core";
 
 export const video = pgTable("video", (t) => ({
-	id: t.serial().primaryKey()
+	id: t.serial().primaryKey(),
+	title: t.text().notNull().unique(),
+	submissionsOpen: t.boolean("submissions_open").notNull().default(true)
 }));
