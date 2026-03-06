194eb9288cf1ad8290e211bff1eb0d6e268e8356
Gurkan <hello@gurkz.me>
2026-03-06 17:15:47 +0100

    remove the `throw` from error(403), causes some weird bug in prod

--- src/lib/api/utils.ts
+++ src/lib/api/utils.ts
@@ -6,5 +6,5 @@ export const GURKANS_USER_ID = "RD12iPBWXphrrOjaRsdNRQCJOd6Z7nbQ";
 export function gurkanOnlyGuard() {
 	const ev = getRequestEvent();
 
-	if (!ev.locals.user || ev.locals.user.id !== GURKANS_USER_ID) throw error(403);
+	if (!ev.locals.user || ev.locals.user.id !== GURKANS_USER_ID) error(403);
 }
