b9ea5cd982b6dc03c3fc8729ac80353e8cc58691
Gurkan <hello@gurkz.me>
2026-04-02 17:03:07 +0200

    profile creation

--- src/lib/api/profiles.remote.ts
+++ src/lib/api/profiles.remote.ts
@@ -1,7 +1,28 @@
-import { query } from "$app/server";
+import { form, query } from "$app/server";
 import { db } from "$lib/server/db";
+import { profile } from "$lib/server/db/schema/profile";
+import * as v from "valibot";
+
+import { gurkanOnlyGuard } from "./utils";
 
 export const getProfiles = query(async () => {
 	const allProfiles = await db.query.profile.findMany();
 	return allProfiles;
 });
+
+export const createProfile = form(
+	v.object({
+		line1: v.string(),
+		line2: v.string()
+	}),
+	async (data) => {
+		gurkanOnlyGuard();
+
+		await db.insert(profile).values({
+			line1: data.line1,
+			line2: data.line2
+		});
+
+		await getProfiles().refresh();
+	}
+);
--- src/routes/admin/profiles/+page.svelte
+++ src/routes/admin/profiles/+page.svelte
@@ -8,16 +8,26 @@
     AlertDescription,
   } from "$lib/components/ui/alert/index.js";
   import CircleAlert from "@lucide/svelte/icons/circle-alert";
-
-  const promise = $derived(getProfiles());
-  const profiles = $derived(await promise);
-
   import {
     Card,
     CardContent,
     CardHeader,
     CardTitle,
   } from "$lib/components/ui/card/index.js";
+  import { createProfile } from "$lib/api/profiles.remote.js";
+  import InputErrors from "$lib/components/form/input-errors.svelte";
+  import { Input } from "$lib/components/ui/input";
+  import Label from "$lib/components/ui/label/label.svelte";
+
+  const promise = $derived(getProfiles());
+  const profiles = $derived(await promise);
+
+  function toErrors(arr: string[]): [string, ...string[]] | null {
+    if (arr.length === 0) return null;
+
+    const [first, ...rest] = arr;
+    return [first, ...rest];
+  }
 </script>
 
 <Button href={resolve("/admin")}>go back</Button>
@@ -34,6 +44,70 @@
     do that.</AlertDescription
   >
 </Alert>
+
+<div>
+  <Card class="my-6 w-72">
+    <CardHeader>
+      <CardTitle>create a new profile</CardTitle>
+    </CardHeader>
+    <CardContent>
+      <form {...createProfile}>
+        <div>
+          <Label
+            class={[
+              !!createProfile.fields.line1.issues() && "text-destructive",
+              "pb-2",
+            ]}>line 1</Label
+          >
+          <Input
+            {...createProfile.fields.line1.as("text")}
+            aria-errormessage="{createProfile.fields.line1.as('text')
+              .name}-error"
+            aria-invalid={!!createProfile.fields.line1.issues()}
+          />
+
+          <InputErrors
+            name={createProfile.fields.line1.as("text").name}
+            errors={toErrors(
+              createProfile.fields.line1
+                .issues()
+                ?.map((value) => value.message) ?? [],
+            )}
+          />
+        </div>
+
+        <br />
+
+        <div>
+          <Label
+            class={[
+              !!createProfile.fields.line2.issues() && "text-destructive",
+              "pb-2",
+            ]}>line 2</Label
+          >
+          <Input
+            {...createProfile.fields.line2.as("text")}
+            aria-errormessage="{createProfile.fields.line2.as('text')
+              .name}-error"
+            aria-invalid={!!createProfile.fields.line2.issues()}
+          />
+
+          <InputErrors
+            name={createProfile.fields.line2.as("text").name}
+            errors={toErrors(
+              createProfile.fields.line2
+                .issues()
+                ?.map((value) => value.message) ?? [],
+            )}
+          />
+        </div>
+
+        <br />
+        <Button type="submit">create</Button>
+      </form>
+    </CardContent>
+  </Card>
+</div>
 
 <div class="grid gap-4 pt-6 sm:grid-cols-1 md:grid-cols-2">
   {#each profiles as profile (profile.id)}
