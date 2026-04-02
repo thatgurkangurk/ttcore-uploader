8506d6886cb8d06128ae6ff698a72bf3be79c251
Gurkan <hello@gurkz.me>
2026-04-02 17:19:10 +0200

    fix error.svelte pages

--- src/routes/submit/+error.svelte
+++ src/routes/submit/+error.svelte
@@ -1,21 +1,26 @@
 <script lang="ts">
-	import { page } from "$app/state";
-	import { Button } from "$lib/components/ui/button/index.js";
-	import { useSession } from "$lib/session.svelte";
+  import { page } from "$app/state";
+  import { Button } from "$lib/components/ui/button/index.js";
+  import { useSession } from "$lib/session.svelte";
 
-	const session = useSession();
+  const session = useSession();
 </script>
 
-{#if page.status === 403 || page.status === 401}
-	<h1 class="text-3xl font-bold tracking-tight md:text-4xl">please sign in to continue</h1>
-	<br />
-	<Button
-		onclick={() =>
-			session.authClient.signIn.social({
-				provider: "discord",
-				loginHint: "yes"
-			})}>sign in with Discord</Button
-	>
-{:else}
-	<h1>error {page.status}: {page.error?.message}</h1>
+{#if page.status === 401}
+  <h1 class="text-3xl font-bold tracking-tight md:text-4xl">
+    please sign in to continue
+  </h1>
+  <br />
+  <Button
+    onclick={() =>
+      session.authClient.signIn.social({
+        provider: "discord",
+        loginHint: "yes",
+      })}>sign in with Discord</Button
+  >
+{:else if page.status === 403}
+  <h1 class="text-3xl font-bold tracking-tight md:text-4xl">
+    {page.error?.message ?? "sorry, but you cannot access this page"}
+  </h1>
+  <h1>error {page.status}: {page.error?.message}</h1>
 {/if}
